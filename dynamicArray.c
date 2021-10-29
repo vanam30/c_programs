9a758a0a8544bfd8c7ddedfab0e706d7acc6f5ec
vanam30 <85978070+vanam30@users.noreply.github.com>
2021-10-29 23:51:57 +0530

    Dynamic array implementation

---
+++ dynamicArray.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    int *a, n, i;
+    printf("\nEnter the size of the array : ");
+    scanf("%d", &n);
+    a = (int *) malloc(sizeof(int) * n);
+    for(i = 0; i < n; i++){
+        a[i] = i * 10;
+    }
+    for(i = 0; i < n; i++){
+        printf("\n%d", a[i]);
+    }
+	return 0;
+}
